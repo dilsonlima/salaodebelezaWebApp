1e6ee3b4c3eda18928e56c498584d00a49c4ba1c
Dilsinhu <hayashi.dilson@gmail.com>
2025-03-09 01:35:09 -0300

    melhorias

--- app.py
+++ app.py
@@ -17,17 +17,15 @@ def init_db():
                 service_description TEXT NOT NULL,
                 service_value REAL NOT NULL,
                 date TEXT NOT NULL,
-                time TEXT NOT NULL,
+                start_time TEXT NOT NULL,  -- Novo campo: horário de início
+                end_time TEXT NOT NULL,    -- Novo campo: horário de fim
                 recurrence TEXT NOT NULL,
                 color TEXT NOT NULL,
-                pago BOOLEAN DEFAULT 0  -- Nova coluna
+                pago BOOLEAN DEFAULT 0
             )
         ''')
         conn.commit()
-
 init_db()
-
-
 
 #Adicionar nova rota para resumo do dia
 from datetime import datetime, time
@@ -38,14 +36,14 @@ def index():
         with sqlite3.connect(DATABASE) as conn:
             cursor = conn.cursor()
             today = datetime.now().strftime('%Y-%m-%d')
-            cursor.execute('SELECT * FROM appointments WHERE date = ? ORDER BY time', (today,))
+            cursor.execute('SELECT * FROM appointments WHERE date = ? ORDER BY start_time', (today,))
             appointments = cursor.fetchall()
 
             # Remove agendamentos com horários já passados
             current_time = datetime.now().time()
             filtered_appointments = []
             for appointment in appointments:
-                appointment_time = datetime.strptime(appointment[5], '%H:%M').time()  # appointment[5] é a hora
+                appointment_time = datetime.strptime(appointment[5], '%H:%M').time()  # appointment[5] é o start_time
                 if appointment_time >= current_time:
                     filtered_appointments.append(appointment)
 
@@ -55,6 +53,7 @@ def index():
 
     return render_template('index.html', appointments=filtered_appointments)
 
+
 # Rota para adicionar agendamento
 @app.route('/add', methods=['GET', 'POST'])
 def add_appointment():
@@ -63,8 +62,10 @@ def add_appointment():
         service_description = request.form['service_description']
         service_value = request.form['service_value']
         date = request.form['date']
-        time = request.form['time']
+        start_time = request.form['start_time']
+        end_time = request.form['end_time']
         recurrence = request.form['recurrence']
+        pago = 0
 
         # Define a cor baseada no mês
         month = datetime.strptime(date, '%Y-%m-%d').month
@@ -78,17 +79,17 @@ def add_appointment():
                 if recurrence == '1x':
                     # Insere apenas uma vez
                     cursor.execute('''
-                        INSERT INTO appointments (client_name, service_description, service_value, date, time, recurrence, color)
-                        VALUES (?, ?, ?, ?, ?, ?, ?)
-                    ''', (client_name, service_description, service_value, date, time, recurrence, color))
+                        INSERT INTO appointments (client_name, service_description, service_value, date, start_time, end_time, recurrence, color,pago)
+                        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+                    ''', (client_name, service_description, service_value, date, start_time, end_time, recurrence, color,pago))
                 else:
                     # Insere as recorrências
                     for i in range(4):
                         new_date = calculate_recurrence_date(date, recurrence, i)
                         cursor.execute('''
-                            INSERT INTO appointments (client_name, service_description, service_value, date, time, recurrence, color)
-                            VALUES (?, ?, ?, ?, ?, ?, ?)
-                        ''', (client_name, service_description, service_value, new_date.strftime('%Y-%m-%d'), time, recurrence, color))
+                            INSERT INTO appointments (client_name, service_description, service_value, date, start_time, end_time, recurrence, color,pago)
+                            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+                        ''', (client_name, service_description, service_value, new_date.strftime('%Y-%m-%d'), start_time, end_time, recurrence, color,pago))
                 conn.commit()
         except sqlite3.Error as e:
             print(f"Erro no banco de dados: {e}")
@@ -96,7 +97,8 @@ def add_appointment():
         return redirect(url_for('view_appointments'))
 
     return render_template('add_appointment.html')
-
+    
+   
 # Função para calcular a data de recorrência
 def calculate_recurrence_date(start_date, recurrence, iteration):
     start_date = datetime.strptime(start_date, '%Y-%m-%d')
@@ -117,7 +119,7 @@ def view_appointments():
     try:
         with sqlite3.connect(DATABASE) as conn:
             cursor = conn.cursor()
-            cursor.execute('SELECT * FROM appointments ORDER BY date, time')
+            cursor.execute('SELECT * FROM appointments ORDER BY date, start_time')  # Substitua 'time' por 'start_time'
             appointments = cursor.fetchall()
 
             # Agrupa os agendamentos por mês e calcula a soma dos valores
@@ -152,27 +154,28 @@ from flask import jsonify  # Certifique-se de que jsonify está importado
 @app.route('/update_payment_status', methods=['POST'])
 def update_payment_status():
     try:
-        # Obtém os dados JSON da requisição
         data = request.get_json()
         if not data or 'appointmentIds' not in data:
             return jsonify({'success': False, 'error': 'Dados inválidos'}), 400
 
         appointment_ids = data['appointmentIds']
 
-        # Atualiza o status de pagamento no banco de dados
         with sqlite3.connect(DATABASE) as conn:
             cursor = conn.cursor()
-            # Marca como pago os agendamentos selecionados
             if appointment_ids:
+                # Marca como pago os agendamentos selecionados
                 cursor.execute(
                     'UPDATE appointments SET pago = 1 WHERE id IN ({})'.format(
                         ','.join(['?'] * len(appointment_ids))
                     ), appointment_ids)
-            # Marca como não pago os agendamentos não selecionados
-            cursor.execute(
-                'UPDATE appointments SET pago = 0 WHERE id NOT IN ({})'.format(
-                    ','.join(['?'] * len(appointment_ids)) if appointment_ids else '1=1'
-                ), appointment_ids if appointment_ids else [])
+                # Marca como não pago os agendamentos não selecionados
+                cursor.execute(
+                    'UPDATE appointments SET pago = 0 WHERE id NOT IN ({})'.format(
+                        ','.join(['?'] * len(appointment_ids))
+                    ), appointment_ids)
+            else:
+                # Se nenhum ID for enviado, marca todos como não pagos
+                cursor.execute('UPDATE appointments SET pago = 0')
             conn.commit()
 
         return jsonify({'success': True})
